2843401eced12d450487426ec17651940474929e
marialclarie <marialclarie@gmail.com>
2021-07-27 23:20:56 +0300

    Added jquerry to toggle the what we do section

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+   $('.info1').click(function(){
+       $('.info1-sub').toggle();
+       $('#hide1').toggle();
+   })
+   $('.info2').click(function(){
+    $('.info2-sub').toggle();
+    $('#hide2').toggle();
+    })
+    $('.info3').click(function(){
+        $('.info3-sub').toggle();
+        $('#hide3').toggle();
+    })
+
+  
+});
